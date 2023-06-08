325981a936a1d48283cf581739c940caec24b032
rtrikha <raunakstrikha@gmail.com>
2023-06-08 22:59:29 +0530

    adding theme switcher

--- js/script.js
+++ js/script.js
@@ -9,35 +9,41 @@ function toggleValue() {
 
 	function animateInLight(callback) {
 		var svg = document.getElementById('light-mode-svg');
-		svg.style.transform = 'rotate(2deg) translateX(1px)';
-		svg.style.transition = 'all 0.2s';
+		svg.style.transform = 'rotate(1deg) translateX(1px)';
+		svg.style.transition = 'all 0.4s';
 		setTimeout(callback, 100);
 	}
 
 	function animateOutLight() {
 		var svg = document.getElementById('light-mode-svg');
-		svg.style.transform = 'rotate(-2deg)';
-		svg.style.transition = 'all 0.2s';
+		svg.style.transform = 'rotate(-1deg)';
+		svg.style.transition = 'all 0.4s';
 	}
 
 	function animateInDark(callback) {
 		var svg = document.getElementById('dark-mode-svg');
-		svg.style.transform = 'rotate(-2deg) translateX(-1px)';
-		svg.style.transition = 'all 0.2s';
+		svg.style.transform = 'rotate(-1deg) translateX(-1px)';
+		svg.style.transition = 'all 0.4s';
 		setTimeout(callback, 100);
 	}
 
 	function animateOutDark() {
 		var svg = document.getElementById('dark-mode-svg');
-		svg.style.transform = 'rotate(2deg)';
-		svg.style.transition = 'all 0.2s';
+		svg.style.transform = 'rotate(1deg)';
+		svg.style.transition = 'all 0.4s';
 	}
 
 	function checkState() {
 		if (toggle) {
 			animateInLight(animateOutLight);
+			applyColourTheme('stockDark');
+			document.getElementById('light-mode-svg').style.filter = 'invert(1)';
+			document.getElementById('light-mode-svg').style.opacity = '0.6';
 		} else {
 			animateInDark(animateOutDark);
+			applyColourTheme('stockLight');
+			document.getElementById('light-mode-svg').style.filter = 'invert(0)';
+			document.getElementById('light-mode-svg').style.opacity = '1';
 		}
 	}
 
---
+++ js/theme.js
@@ -0,0 +1,38 @@
+const Themes = {
+	stockLight: {
+		appBackground: '#E4D0BC',
+		foregroundHigh: '#000000',
+		foregroundMed: '#000000e6',
+		foregroundLow: '#00000080',
+		backgroundHigh: '#E9DACC',
+		backgroundMed: '#1D1D1F',
+		backgroundLow: ' #000000',
+		toneForeground: '#E9DACC',
+		toneBackground: '#00000026',
+	},
+	stockDark: {
+		appBackground: '#000010',
+		foregroundHigh: '#ECE7DA',
+		foregroundMed: '#ECE7DAe6',
+		foregroundLow: '#ECE7DA80',
+		backgroundHigh: '#161616',
+		backgroundMed: '#1D1D1F',
+		backgroundLow: ' #000000',
+		toneForeground: '#ECE7DA',
+		toneBackground: '#ffffff26',
+	},
+};
+
+window.applyColourTheme = function (themeName) {
+	document.documentElement.style.setProperty('--a-b', Themes[themeName].appBackground);
+	document.documentElement.style.setProperty('--f-h', Themes[themeName].foregroundHigh);
+	document.documentElement.style.setProperty('--f-m', Themes[themeName].foregroundMed);
+	document.documentElement.style.setProperty('--f-l', Themes[themeName].foregroundLow);
+	document.documentElement.style.setProperty('--b-h', Themes[themeName].backgroundHigh);
+	document.documentElement.style.setProperty('--b-m', Themes[themeName].backgroundMed);
+	document.documentElement.style.setProperty('--b-l', Themes[themeName].backgroundLow);
+	document.documentElement.style.setProperty('--t-f', Themes[themeName].toneForeground);
+	document.documentElement.style.setProperty('--t-b', Themes[themeName].toneBackground);
+};
+
+applyColourTheme('stockLight');
